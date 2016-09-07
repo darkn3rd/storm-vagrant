8d7ec4352a2c9bc06fa71626a2b6dcfc8bd8ef1a
Joaquin Menchaca <jmenchaca@gobalto.com>
2016-09-07 10:59:28 -0700

    nginx proxy scripts

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-SCRIPT_PATH="/vagrant/scripts/"
-. ${SCRIPT_PATH}/lib/nginx.src
 
+# Configure Local Vagrant Paths
+CONFIGFILE=${VAGRANT_CONFIG:-"/vagrant/config/default.hosts"}
+SCRIPT_PATH="/vagrant/scripts"
+SCRIPTLIB="${SCRIPT_PATH}/libs"
+
+# Import Libraries
+. ${SCRIPTLIB}/nginx.src  # install_nginx(), remote_site(), enable_site()
+. ${SCRIPTLIB}/base.src   # get_ipaddress()
+
+# Install nginx from vendor PPA
 install_nginx
+
+# Remove Default Site
 remove_site 'default'
-enable_site 80 "http://192.168.54.4:8080" 'storm.dev' 'nimbus.dev'
-enable_site 8000 "http://192.168.54.5:8000" 'supervisor1.dev'
-enable_site 8000 "http://192.168.54.6:8000" 'supervisor2.dev'
+
+# Create Sites based on Configuration (e.g. config/default.hosts)
+create_site 80 "http://$(get_ipaddress ${CONFIGFILE} nimbus.dev):8080" 'storm.dev' 'nimbus.dev'
+create_site 8000 "http://$(get_ipaddress ${CONFIGFILE} supervisor1.dev):8000" 'supervisor1.dev'
+create_site 8000 "http://$(get_ipaddress ${CONFIGFILE} supervisor2.dev):8000" 'supervisor2.dev'
+
+# Enable newly created sites
+enable_site nimbus.dev
+enable_site supervisor1.dev
+enable_site supervisor2.dev
