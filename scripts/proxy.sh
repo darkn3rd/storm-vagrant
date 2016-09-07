e5b7ab585faeedcb5f1e6e7ffc90c19756650b30
Joaquin Menchaca <jmenchaca@gobalto.com>
2016-09-06 22:35:58 -0700

    updated - able to get nimbus working, working on precise config for backend servers

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,2 +1,42 @@
 #!/bin/sh
 SCRIPT_PATH="/vagrant/scripts/"
+
+export DEBIAN_FRONTEND=noninteractive
+add-apt-repository ppa:nginx/stable
+apt-get -qq update && apt-get -y install nginx
+
+# config_ngix_site(upstream, server_names)
+#
+# each list does lookup of
+# server_name $@
+# get_ipaddress $1
+
+cat <<-'STORMUI_CONF' > /etc/nginx/sites-available/storm
+server{
+  server_name storm.dev nimbus.dev;
+  location / {
+    proxy_pass http://192.168.54.4:8080;
+  }
+}
+
+server{
+  server_name supervisor1.dev;
+  location / {
+    proxy_pass http://192.168.54.5;
+  }
+}
+
+server{
+  server_name supervisor2.dev;
+  location / {
+    proxy_pass http://192.168.54.6;
+  }
+}
+
+STORMUI_CONF
+
+ln -s /etc/nginx/sites-available/storm storm
+
+# if service nginx configtest
+
+sudo service nginx reload
