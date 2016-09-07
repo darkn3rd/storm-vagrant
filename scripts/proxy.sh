2c1f418e2c0f14ad6c3e115a30b321ab6afa4bbf
Joaquin Menchaca <jmenchaca@gobalto.com>
2016-09-07 06:05:01 -0700

    proxy - initial nginx automation

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,44 +1,9 @@
 #!/bin/sh
 SCRIPT_PATH="/vagrant/scripts/"
+. ${SCRIPT_PATH}/lib/nginx.src
 
-export DEBIAN_FRONTEND=noninteractive
-add-apt-repository ppa:nginx/stable
-apt-get -qq update && apt-get -y install nginx
-
-# config_ngix_site(upstream, server_names)
-#
-# each list does lookup of
-# server_name $@
-# get_ipaddress $1
-
-cat <<-'STORMUI_CONF' > /etc/nginx/sites-available/storm
-server {
-  server_name storm.dev nimbus.dev;
-  location / {
-    proxy_pass http://192.168.54.4:8080;
-  }
-}
-
-server {
-  listen 8000;
-  server_name supervisor1.dev;
-  location / {
-    proxy_pass http://192.168.54.5:8000;
-  }
-}
-
-server {
-  listen 8000;
-  server_name supervisor2.dev;
-  location / {
-    proxy_pass http://192.168.54.6:8000;
-  }
-}
-
-STORMUI_CONF
-
-ln -s /etc/nginx/sites-available/storm storm
-
-# if service nginx configtest
-
-sudo service nginx reload
+install_nginx
+remove_site 'default'
+enable_site 80 "http://192.168.54.4:8080" 'storm.dev' 'nimbus.dev'
+enable_site 8000 "http://192.168.54.5:8000" 'supervisor1.dev'
+enable_site 8000 "http://192.168.54.6:8000" 'supervisor2.dev'
